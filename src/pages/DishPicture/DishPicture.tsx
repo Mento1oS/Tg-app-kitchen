5d1129b5ba9f2b3744f0d17be9ee68e354c72938
Artem_Mentolos <mentolosscg@yandex.ru>
2024-06-02 22:40:36 +0300

    fourth page

--- src/pages/DishPicture/DishPicture.tsx
+++ src/pages/DishPicture/DishPicture.tsx
@@ -1,7 +1,9 @@
 /** @format */
-
-const DishPicture = () => {
-  return <div>DishPicture</div>;
+import style from './DishPicture.module.css'
+const DishPicture = (props:{picture: string}) => {
+  return <div className={style.conatiner}>
+    <img className={style.image} src={props.picture} alt="dish"/>
+  </div>;
 };
 
 export default DishPicture;
--- src/pages/SoloWaitingRoom/SoloWaitingRoom.tsx
+++ src/pages/SoloWaitingRoom/SoloWaitingRoom.tsx
@@ -1,10 +1,28 @@
 /** @format */
 import style from './SoloWaitingRoom.module.css'
+import {Dispatch, SetStateAction, useEffect, useState} from "react";
+import DishPicture from "../DishPicture/DishPicture.tsx";
+import {useNavigate} from "react-router-dom";
 
 const SoloWaitingRoom = () => {
-  return <div className={style.container}>
-
-  </div>;
+    const [counter, setCounter]: [counter: number, useCounter: Dispatch<SetStateAction<number>>] = useState(3);
+    const navigate = useNavigate();
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter(counter - 1);
+            // if(counter<=0){
+            //   navigate('/solo/game/1');
+            // }
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [counter]);
+    return <div className={style.container}>
+        <div className={style.picture}>
+            <DishPicture picture={'/dish_picture.png'}/>
+        </div>
+        <div className={style.header}>до старта</div>
+        <div className={String(style.counter)}>{counter}</div>
+    </div>;
 };
 
 export default SoloWaitingRoom;
