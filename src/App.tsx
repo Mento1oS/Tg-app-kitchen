80482c5dab7e70b88c188c8577808f5a2005dc6b
Artem_Mentolos <mentolosscg@yandex.ru>
2024-06-02 12:31:18 +0300

    rebuild test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 /** @format */
 
 import "./App.css";
+import {useEffect} from "react";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+const telegram = window.Telegram.WebApp;
 
 function App() {
+  useEffect(()=>{
+    telegram.ready();
+  },[])
+
+  // const onClose = ()=>{
+  //   telegram.close();
+  // }
+
   return <div></div>;
 }
 
