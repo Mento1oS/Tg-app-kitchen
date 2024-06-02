62fd1b714c1b4fccb6ee93e35c8c1af8e3e49302
Artem_Mentolos <mentolosscg@yandex.ru>
2024-06-02 13:06:54 +0300

    routing added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@
 
 import "./App.css";
 import {useEffect} from "react";
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-const telegram = window.Telegram.WebApp;
+import useTelegram from "./utils/useTelegram.ts";
+import Header from "./pages/Header/Header.tsx";
+import {Route, Routes} from "react-router-dom";
+import Main from "./pages/Main/Main.tsx";
+import SoloLanding from "./pages/SoloLanding/SoloLanding.tsx";
+import SoloTask from "./pages/SoloTask/SoloTask.tsx";
+import SoloWaitingRoom from "./pages/SoloWaitingRoom/SoloWaitingRoom.tsx";
 
 function App() {
+  const telegram = useTelegram()
   useEffect(()=>{
     telegram.ready();
   },[])
@@ -16,7 +20,16 @@ function App() {
   //   telegram.close();
   // }
 
-  return <div></div>;
+  return <div>
+    <Header/>
+    <Routes>
+      <Route index element={<Main/>}/>
+      <Route path='/solo' element={<SoloLanding/>}/>
+      <Route path='/solo/task' element={<SoloTask/>}/>
+      <Route path='/solo/game' element={<SoloWaitingRoom/>}/>
+      <Route path='/solo/game/:id'/>
+    </Routes>
+  </div>;
 }
 
 export default App;
---
+++ src/utils/useTelegram.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+const telegram = window.Telegram.WebApp
+const useTelegram = ()=>{
+    return telegram;
+}
+
+export default useTelegram;
