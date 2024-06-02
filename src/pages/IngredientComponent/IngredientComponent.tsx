7d5ce44a26676b04a1e2636bfcacc173d4b7f967
Artem_Mentolos <mentolosscg@yandex.ru>
2024-06-03 01:55:21 +0300

    finilize logic

--- src/pages/IngredientComponent/IngredientComponent.tsx
+++ src/pages/IngredientComponent/IngredientComponent.tsx
@@ -1,9 +1,13 @@
 /** @format */
 import style from './IngredientComponent.module.css'
-const IngredientComponent = (props:{picture: string}) => {
-  return <div className={style.container}>
-    <img src={props.picture} alt="ingredient"/>
-  </div>;
+
+const IngredientComponent = (props: { picture: string, timer: number }) => {
+    return <div className={style.container}>
+        <img src={props.picture} alt="ingredient"/>
+        <div className={style.counter}>
+            <div className={style.text}>{props.timer}</div>
+        </div>
+    </div>;
 };
 
 export default IngredientComponent;
--- src/pages/SoloInGamePage/SoloInGamePage.tsx
+++ src/pages/SoloInGamePage/SoloInGamePage.tsx
@@ -3,7 +3,7 @@
 import style from './SoloIngamePage.module.css'
 import IngredientComponent from "../IngredientComponent/IngredientComponent.tsx";
 import ApprovePicture from "../ApprovePicture/ApprovePicture.tsx";
-import {Dispatch, SetStateAction} from "react";
+import {Dispatch, SetStateAction, useEffect, useRef, useState} from "react";
 import {Params, useNavigate, useParams} from "react-router-dom";
 
 const SoloInGamePage = (props: {
@@ -11,17 +11,30 @@ const SoloInGamePage = (props: {
 }) => {
     const params: Readonly<Params<string>> = useParams();
     const navigate = useNavigate();
+    const [timer, setTimer]:[number, Dispatch<SetStateAction<number>>] = useState(30)
     const stage = params?.stage;
+    const interval = useRef(0);
+    useEffect(() => {
+        interval.current = setInterval(() => {
+            setTimer(timer - 1);
+            if(timer<=0){
+                disapproveFunc();
+            }
+        }, 1000);
+        return () => clearInterval(interval.current);
+    }, [timer]);
     if(!stage) return;
     const onClick = (approved: boolean, answers: NonNullable<unknown>, setAnswers:Dispatch<SetStateAction<NonNullable<unknown>>>, stage:string) => {
         setAnswers({...answers, [stage]: approved});
+        setTimer(30);
         navigate(`/solo/game/${Number(stage)+1}`)
     }
     const disapproveFunc = onClick.bind(null, false, props.answers, props.setAnswers, stage);
     const approveFunc = onClick.bind(null, true, props.answers, props.setAnswers, stage);
+
     return <div className={style.container}>
         <div className={style.picture}>
-            <IngredientComponent picture={'/ingredient_pic.png'}/>
+            <IngredientComponent timer={timer} picture={'/ingredient_pic.png'}/>
         </div>
         <div className={style.ingredient}>свиная вырезка</div>
         <div className={style.votebar}>
