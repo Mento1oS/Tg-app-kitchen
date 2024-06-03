095af4bd3c638e0357032c29807e3c6035893f91
Artem_Mentolos <mentolosscg@yandex.ru>
2024-06-03 12:26:33 +0300

    animation set up

--- src/pages/SoloLanding/SoloLanding.tsx
+++ src/pages/SoloLanding/SoloLanding.tsx
@@ -3,7 +3,7 @@ import style from './SoloLanding.module.css'
 import SubmitButton from "../SubmitButton/SubmitButton.tsx";
 import { motion } from 'framer-motion';
 function SoloLanding(){
-  return <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className={style.container}>
+  return <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0, transform: 'rotate3d(0, 1, 0, 3.142rad'}} className={style.container}>
     <div className={style.layers}>
       <div className={style.task}>
         Приготовь мне мясо по-французски
